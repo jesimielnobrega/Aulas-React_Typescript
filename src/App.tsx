5b54970eee34497314922c2365c3895705286c1f
Jesimiel <jesimielnobrega25@gmail.com>
2024-01-19 22:42:02 +0100

    Eliminando CSS desnecessário

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import './app.css';
+
 
 function App() {
 
@@ -6,6 +8,7 @@ function App() {
       <h1>
         Hello World
       </h1>
+      <img src="https://imgs.search.brave.com/YPEzUSkRPys3zfh9O40efYcpHQkWq73ktJUMHVSxmUU/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuZnJlZWltYWdl/cy5jb20vaW1hZ2Vz/L2xhcmdlLXByZXZp/ZXdzLzYxMC9yYW5k/b20tMTU3NDM5MS5q/cGc_Zm10" alt="" />
     </>
   )
 }
