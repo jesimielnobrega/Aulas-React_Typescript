95c03912469510cbb5c92721ff970d51057d2c34
Jesimiel <jesimielnobrega25@gmail.com>
2024-01-20 23:58:28 +0100

    Formatando o index do routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,11 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 
 
-export const AppRoutes =()=>{
-    return(
+export const AppRoutes = () => {
+    return (
         <Routes>
-            <Route path="/pagina-inicial" element={<p>Página inicial</p>}/>
-            <Route path="*" element={<Navigate to="/pagina-inicial"/>}/>
+            <Route path="/pagina-inicial" element={<p>Página inicial</p>} />
+            <Route path="*" element={<Navigate to="/pagina-inicial" />} />
         </Routes>
     );
 }
