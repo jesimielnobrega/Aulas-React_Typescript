db41296540f5f04439b77a84e3340053664b7a91
Jesimiel <jesimielnobrega25@gmail.com>
2024-01-20 23:56:36 +0100

    Adicionei o react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import './app.css';
-
-
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
 function App() {
-
   return (
-    <>
-      <h1>
-        Hello World
-      </h1>
-      <img src="https://imgs.search.brave.com/YPEzUSkRPys3zfh9O40efYcpHQkWq73ktJUMHVSxmUU/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuZnJlZWltYWdl/cy5jb20vaW1hZ2Vz/L2xhcmdlLXByZXZp/ZXdzLzYxMC9yYW5k/b20tMTU3NDM5MS5q/cGc_Zm10" alt="" />
-    </>
-  )
+    <BrowserRouter>
+      <AppRoutes/>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
